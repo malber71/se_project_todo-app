09b2a5c8634577333d722b900ada773d3e9217a6
malber71 <michaelalber71@gmail.com>
2025-06-08 11:57:56 -0700

    working files refactoring for classes

---
+++ components/FormValidator.js
@@ -0,0 +1,46 @@
+class FormValidator {
+    constructor(settings, formEl){
+        this._inputSelector = settings.inputSelector;
+        this._submitButtonSelector = settings.submitButtonSelector;
+        this._errorClass = settings.errorClass;
+        this._inputErrorClass = settings.inputErrorClass;
+        this._inactiveButtonClass = settings.inactiveButtonClass;
+        this._formEl = formEl;
+    }
+
+    _checkInputValidity(){
+
+    }
+
+    _toggleButtonState(){
+        console.log("success");
+    }
+
+    _setEventListeners(){
+        this._inputList = Array.from(
+            this._formEl.querySelectorAll(this._inputSelector),
+          );
+          const buttonElement = this._formEl.querySelector(
+            this._submitButtonSelector,
+          );
+        
+          this._toggleButtonState(inputList, buttonElement );
+        
+          this._inputList.forEach((inputElement) => {
+            inputElement.addEventListener("input", () => {
+              this._checkInputValidity(inputElement);
+              this._toggleButtonState(inputList, buttonElement, settings);
+            });
+          });
+
+    }
+
+    enableValidation(){
+        this._formEl.addEventListener("submit", (evt) => {
+          evt.preventDefault();
+        }); 
+        this._setEventListeners();
+    }
+}
+
+export default FormValidator; 
