0fe260ac3162fa367f5ab131fa3148a61bc8aa5b
malber71 <michaelalber71@gmail.com>
2025-06-15 00:05:33 -0700

    correcting line 64 to remove passed arguments

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -61,7 +61,7 @@ class FormValidator {
     this._inputList.forEach((inputElement) => {
       inputElement.addEventListener("input", () => {
         this._checkInputValidity(inputElement);
-        this._toggleButtonState(this._inputList, this._buttonElement);
+        this._toggleButtonState();
       });
     });
   }
