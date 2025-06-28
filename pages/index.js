5f0610b0d1e5e4874844fb8b29b24599d5ef5113
Michael Alber <michaelalber71@gmail.com>
2025-06-28 16:37:21 -0700

    sprint 8 project ready for submission

--- pages/index.js
+++ pages/index.js
@@ -11,37 +11,45 @@ const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopupEl = document.querySelector("#add-todo-popup");
 const addTodoForm = document.forms["add-todo-form"];
 const addTodoCloseBtn = addTodoPopupEl.querySelector(".popup__close");
-// const todoTemplate = document.querySelector("#todo-template");
-// const todosList = document.querySelector(".todos__list");
-
-const todoCounter = new TodoCounter();
+const todoCounter = new TodoCounter(initialTodos, ".counter__text");
 
 const addTodoPopup = new PopupWithForm({
   popupSelector: "#add-todo-popup",
   handleFormSubmit: (inputValues) => {
     const name = inputValues["name"];
-    const dateValues = inputValues["date"]
-     let date = new Date(dateValues);
-       if (dateValues === "") {
-    date = "";
-  } else {
-    date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
-  }
-  
+    const dateValues = inputValues["date"];
+    let date = new Date(dateValues);
+    if (dateValues === "") {
+      date = "";
+    } else {
+      date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+    }
 
     const id = uuidv4();
     const values = { name, date, id };
     renderTodo(values);
     newTodoformValidator.resetValidation();
-
+    todoCounter.updateTotal(true);
     addTodoPopup.close();
-      },
+  },
 });
 addTodoPopup.setEventListeners();
 
+function handleCheck(completed) {
+  todoCounter.updateCompleted(completed);
+}
+
+function handleDelete(completed) {
+  if (completed) {
+    todoCounter.updateCompleted(false);
+  }
+  todoCounter.updateTotal(false);
+}
+
 const generateTodo = (data) => {
-  const todo = new Todo(data, "#todo-template");
+  const todo = new Todo(data, "#todo-template", handleCheck, handleDelete);
   const todoElement = todo.getView();
+
   return todoElement;
 };
 
@@ -55,54 +63,14 @@ const section = new Section({
 
 section.renderItems();
 
-// const openModal = (modal) => {
-//   modal.classList.add("popup_visible");
-// };
-
-// const closeModal = (modal) => {
-//   modal.classList.remove("popup_visible");
-// };
-
-// The logic in this function should all be handled in the Todo class.
-
 addTodoButton.addEventListener("click", () => {
   addTodoPopup.open();
 });
-
-// addTodoCloseBtn.addEventListener("click", () => {
-//   addTodoPopup.close();
-// });
 
 const renderTodo = (item) => {
   const todo = generateTodo(item);
   section.addItem(todo);
 };
-
-// addTodoForm.addEventListener("submit", (evt) => {
-//   evt.preventDefault();
-//   const name = evt.target.name.value;
-//   const dateInput = evt.target.date.value;
-
-//   // Create a date object and adjust for timezone
-//   let date = new Date(dateInput);
-
-//   if (dateInput === "") {
-//     date = "";
-//   } else {
-//     date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
-//   }
-
-//   const id = uuidv4();
-//   const values = { name, date, id };
-//   renderTodo(values);
-//   newTodoformValidator.resetValidation();
-
-//   addTodoPopup.close();
-// });
-
-// initialTodos.forEach((item) => {
-//   renderTodo(item);
-// });
 
 const newTodoformValidator = new FormValidator(validationConfig, addTodoForm);
 newTodoformValidator.enableValidation();
